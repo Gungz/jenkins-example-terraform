0c95c67fa98028f1e08b009de0086f114a31d167
Agung Sidharta So <agungso@amazon.id>
2022-06-08 19:11:40 +0700

    Create S3 in ApplDev account

--- s3.tf
+++ s3.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket" "a" {
+  bucket = "our-tf-test-bucket-212316051817"
+
+  tags = {
+    Name        = "Our bucket"
+    Environment = "Dev"
+  }
+}
